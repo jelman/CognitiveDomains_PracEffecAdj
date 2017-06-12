50dbbb553a3c7630af621db1f6b1e0d9833574d0
jelman <jelman523@gmail.com>
2017-06-12 11:52:34 -0700

    Updated to create cognitive domains from practice effect corrected data

--- CognitiveDomainsV1V2_PE.R
+++ CognitiveDomainsV1V2_PE.R
@@ -17,314 +17,211 @@
 ################################################################################
 
 
-library(sjmisc)
 library(dplyr)
 library(psych)
+
+## Create function to scale by both timepoints by VETSA 1 mean and sd ##
+scaleVals = function(df,varV1) {
+  varV2 = paste0(varV1, "_V2p")
+  zvarV1 = paste0("z", varV1)
+  zvarV2 = paste0("z", varV2)
+  df[[zvarV1]] = scale(df[[varV1]], center=T, scale=T)
+  meanVal = attr(df[[zvarV1]], which="scaled:center")
+  sdVal = attr(df[[zvarV1]], which="scaled:scale")
+  df[[zvarV2]] = scale(df[[varV2]], center=meanVal, scale=sdVal)
+  df
+}
 
 # Load vetsa 1 and 2 practice effect corrected  data
 df = read.csv("~/netshare/M/PSYCH/KREMEN/Practice Effect Cognition/data/V1V2_CogData_PE.csv")
 
 
-##########################################
-# Replace missing data and trim outliers #
-##########################################
-
 #---------------------#
 # Visual-Spatial Data #
 #---------------------#
 
-## Mental Rotation ##
+# Z-score test scores
+df = scaleVals(df, "MR1COR")
+df = scaleVals(df, "HFTOTCOR")
 
-# vetsa1 MENTAL ROTATION: Total Correct PART1
-df$MR1COR[which(df$ZMENROT==2)] = NA
-df$MR2COR[which(df$ZMENROT==2)] = NA
-# MENTAL ROTATION: Total Correct PARTS 1&2
-df$MRTOTCOR= df$MR1COR + df$MR2COR
-
-hfCols = c("HFIGC1","HFIGC3","HFIGC5")
-df[which(df$ZHF==2), hfCols] = NA
-
-# vetsa1 Hidden Figures Total Correct Parts 1,3,5
-df$HFTOTCOR = rowSums(df[hfCols])
+# Create domain score
+df$zVisSpat = rowMeans(df[c("zMR1COR","zHFTOTCOR")], na.rm=T)
+df$zVisSpat_V2p = rowMeans(df[c("zMR1COR_V2p","zHFTOTCOR_V2p")], na.rm=T)
 
 #--------------------#
 # Abstract Reasoning #
 #--------------------#
 
-df$MTXRAW[which(df$ZMATRIX=="2")] = NA
-df$MTXRAW[which(df$MTXRAW=="0")] = NA
-
 # Square-root transformed MTXRAW
 df$MTXTRAN = -1*sqrt(36 - df$MTXRAW)
+df$MTXTRAN_V2p = -1*sqrt(36 - df$MTXRAW_V2p)
 
+# Z-score test scores
+df = scaleVals(df, "MTXTRAN")
+
+# Create domain score
+df$zAbsReason = as.numeric(df[,"zMTXTRAN"])
+df$zAbsReason_V2p = as.numeric(df[,"zMTXTRAN_V2p"])
 
 #-------------------------------#
 # Short Term and Working Memory #
 #-------------------------------#
 
-## Digit Span ##
-df$dsfraw[which(df$ZDGSPNF=="2")] = NA
-df$dsbraw[which(df$ZDGSPNB=="2")] = NA
-
-## Letter-Number Sequencing ##
-df$lntot[which(df$ZLNS=="2")] = NA
-df$lntot[which(df$lntot=="0")] = NA
-
-## Spatial Span ##
-df$sspfraw[which(df$ZSPSPNF=="2")] = NA
-df$sspbraw[which(df$ZSPSPNB=="2")] = NA
-
-## Reading Span ##
-df$RSATOTrev[which(df$ZRDSPNA=="2")] = NA
-
 # Square-root Transformed Reading Span
-df$RSATOTrevtran = -1*sqrt(46 - df$RSATOTrev)
+# NOTE 1: vetsa1 RSATOT is actually pulled from RSATOTrev in the merge dataset. This equates the two versions of the test.
+# NOTE 2: Tasnform subtracts from 48 which is the max score. Original cognitive domain syntax subtracts RSATOT from 46. 
+df$RSATOTTRAN = -1*sqrt(48 - df$RSATOT)
+df$RSATOTTRAN_V2p = -1*sqrt(48 - df$RSATOT_V2p)
+
+# Z-score test scores
+df = scaleVals(df, "DSFRAW")
+df = scaleVals(df, "DSBRAW")
+df = scaleVals(df, "LNTOT")
+df = scaleVals(df, "SSPFRAW")
+df = scaleVals(df, "SSPBRAW")
+df = scaleVals(df, "RSATOTTRAN")
+
+# Create domain score
+df$zSTWKMem = rowMeans(df[,c("zDSFRAW","zDSBRAW","zLNTOT","zSSPFRAW","zSSPBRAW","zRSATOTTRAN")], na.rm=T)
+df$zSTWKMem_V2p = rowMeans(df[,c("zDSFRAW_V2p","zDSBRAW_V2p","zLNTOT_V2p","zSSPFRAW_V2p",
+                                 "zSSPBRAW_V2p","zRSATOTTRAN_V2p")], na.rm=T)
 
 #-----------------#
 # Episodic Memory #
 #-----------------#
 
-## CVLT ##
-cvltiCols = c("cvatot","CVSDFR")
-df[which(df$ZCVLTI=="2"), cvltiCols] = NA
+# Z-score test scores
+df = scaleVals(df, "CVATOT")
+df = scaleVals(df, "CVSDFR")
+df = scaleVals(df, "CVLDFR")
+df = scaleVals(df, "LMITOT")
+df = scaleVals(df, "LMDTOT")
+df = scaleVals(df, "VRITOT")
+df = scaleVals(df, "VRDTOT")
 
-df$CVLDFR[which(df$ZCVLTD=="2")] = NA
-df$cvatot[which(df$cva5raw=="0")] = NA
-
-cvltfrCols = c("CVSDFR","CVLDFR")
-df[which(df$cvatot=="0"), cvltfrCols] = NA
-
-## Logical Memory ##
-
-logmemCols = c("lmitot","lmdtot")
-df[which(df$ZLMI=="2"), logmemCols] = NA
-
-df$lmdtot[which(df$ZLMD=="2")] = NA
-df$lmdtot[is.na(df$lmitot)] = NA
-
-## Visual Reproduction ##
-
-vrCols = c("vritot","vrdtot")
-df[which(df$ZVRI=="2"), vrCols] = NA
-
-df$vrdtot[which(df$ZVRD=="2")] = NA
-df$vrdtot[is.na(df$vritot)] = NA
-
-#--------------------#
-# Executive Function #
-#--------------------#
-
-## Trails Condition 4 ##
-
-df$TRL4T[which(df$ZTRAIL4=="2")] = NA
-df$TRL4T[is.na(df$TRL2T)] = NA
-df$TRL4T[is.na(df$TRL3T)] = NA
-
-# vetsa1 Trails 4 Adjusted for Trails 2&3
-df$TRL4TADJ = with(df, 
-                             (TRL4T - (TRL2T*0.7752) + (TRL3T*1.0438)))
-# vetsa1 SQRT-Transformed Trails 4 Adjusted
-df$TRL4ADJTRAN = -1*sqrt(80 + df$TRL4TADJ)
-
-## Category Fluency Switching ##
-df$CFCOR[which(df$ZFLUC=="2")] = NA
-df$CSSACC[which(df$ZFLUCS=="2")] = NA
-
-# vetsa1 Category Switching Accuracy - Adjusted
-df$CSSACCADJ = df$CSSACC - (0 + (df$CFCOR*0.1329))
-
-## Stroop ##
-df$STRIT [which(df$STRIT=="1")] = NA
-df$STRIT[which(df$STRIT>90)] = NA
-
-df$strcwraw[which(df$ZSTROOPCW=="2") ] = NA
-df$strcwraw[is.na(df$strwraw)] = NA
-df$strcwraw[is.na(df$strcraw)] = NA
-
-df[which(df$vetsaid=="19885B"),"strcraw"] = 86
-df[which(df$vetsaid=="19885B"),"strcwraw"] = 45
+# Create domain score
+df$zEpsMem = rowMeans(df[,c("zCVATOT","zCVSDFR","zCVLDFR","zLMITOT","zLMDTOT","zVRITOT","zVRDTOT")], na.rm=T)
+df$zEpsMem_V2p = rowMeans(df[,c("zCVATOT_V2p","zCVSDFR_V2p","zCVLDFR_V2p","zLMITOT_V2p",
+                                "zLMDTOT_V2p","zVRITOT_V2p","zVRDTOT_V2p")], na.rm=T)
 
 #----------------#
 # Verbal Fluency #
 #----------------#
 
-df$LFCOR[which(df$ZFLUL=="2")] = NA
-df$CFCOR[which(df$ZFLUC=="2")] = NA
+# Z-score test scores
+df = scaleVals(df, "LFCOR")
+df = scaleVals(df, "CFCOR")
+
+# Create domain scores
+df$zVerbFlu = rowMeans(df[,c("zLFCOR","zCFCOR")], na.rm=T)
+df$zVerbFlu_V2p = rowMeans(df[,c("zLFCOR_V2p","zCFCOR_V2p")], na.rm=T)
 
 #------------------#
 # Processing Speed #
 #------------------#
 
-## Stroop ##
-df$strcraw[which(df$ZSTROOPC=="2")] = NA
-df$strwraw[which(df$ZSTROOPW=="2")] = NA
+# Transform Trails time data so that positive is better performance
+# NOTE: Implausible times (e.g., too long or too short) have already been filtered out
+df$TRL2TRAN = -1*(df$TRL2TLOG)
+df$TRL2TRAN_V2p = -1*(df$TRL2TLOG_V2p)
+df$TRL3TRAN = -1*(df$TRL3TLOG)
+df$TRL3TRAN_V2p = -1*(df$TRL3TLOG_V2p)
 
-## Trails ##
-df$TRL2T[which(df$ZTRAIL2=="2")] = NA
-df$TRL3T[which(df$ZTRAIL3=="2")] = NA
+# Create Z-scores
+df = scaleVals(df, "TRL2TRAN")
+df = scaleVals(df, "TRL3TRAN")
+df = scaleVals(df, "STRWRAW")
+df = scaleVals(df, "STRCRAW")
 
-df$TRL2T[which(df$TRL2T>120)] = NA
-df$TRL3T[which(df$TRL3T>120)] = NA
+# Create domain scores
+df$zProcSpeed = rowMeans(df[,c("zSTRWRAW","zSTRCRAW","zTRL2TRAN","zTRL3TRAN")],na.rm=T)
+df$zProcSpeed_V2p = rowMeans(df[,c("zSTRWRAW_V2p","zSTRCRAW_V2p","zTRL2TRAN_V2p","zTRL3TRAN_V2p")],na.rm=T)
 
-# Log Transformed Trails
-df$TRL2TRAN = -1*log(df$TRL2T)
-df$TRL3TRAN = -1*log(df$TRL3T)
+#---------------------------------------#
+# Executive Function : Trails Switching #
+#---------------------------------------#
 
+# Adjust Trails Condition 4 for Trails 2 and 3
+df$TRL4TLOG[is.na(df$TRL2TLOG)] = NA
+df$TRL4TLOG[is.na(df$TRL3TLOG)] = NA
+df$TRL4TLOG_V2p[is.na(df$TRL2TLOG_V2p)] = NA
+df$TRL4TLOG_V2p[is.na(df$TRL3TLOG_V2p)] = NA
 
-#########################################################
-# Creating standardized scores and composite scores.    #
-# Standardization is based off of VETSA1 Means and SDs  #
-#########################################################
+# NOTE 1: Log transformed variables are used to adjust Trails 4, 
+# thus square-root transform used in original syntax is no longer necessary.
+# NOTE 2: Add intercept back in so mean differences between waves are maintained. Otherwise residuals are de-meaned.
+mod.TRL4TLOG = lm(TRL4TLOG ~ TRL2TLOG + TRL3TLOG, data=df, na.action = "na.exclude")
+mod.TRL4TLOG_V2p = lm(TRL4TLOG_V2p ~ TRL2TLOG_V2p + TRL3TLOG_V2p, data=df, na.action = "na.exclude")
+df$TRL4ADJTRAN = -1 * (residuals(mod.TRL4TLOG) + coef(mod.TRL4TLOG)[["(Intercept)"]])
+df$TRL4ADJTRAN_V2p = -1 * (residuals(mod.TRL4TLOG_V2p) + coef(mod.TRL4TLOG_V2p)[["(Intercept)"]])
 
-## Create function to save mean and SD of all variables ##
-addScaleVals = function(df,varname, x) {
-  meanVal = attr(x, which="scaled:center")
-  sdVal = attr(x, which="scaled:scale")
-  rbind(df, data.frame(Variable=varname, Mean=meanVal, SD=sdVal))
-}
+# Z-score test scores
+df = scaleVals(df, "TRL4ADJTRAN")
 
-## Initialize dataframe to hold means and SDs # #
-scaleValues = data.frame()
+# Create domain scores
+df$zExecTrailsSwitch = as.numeric(df[,"zTRL4ADJTRAN"])
+df$zExecTrailsSwitch_V2p = as.numeric(df[,"zTRL4ADJTRAN_V2p"])
 
+#-----------------------------------------#
+# Executive Function : Category Switching #
+#-----------------------------------------#
 
-# Vetsa1 Verbal Ability 
-df$zvoctran = scale(df$voctran)
-scaleValues = addScaleVals(scaleValues, "voctran", df$zvoctran)
+# Adjust Category Switching for Category Fluency
+mod.CSSACC = lm(CSSACC ~ CFCOR, data=df, na.action = "na.exclude")
+mod.CSSACC_V2p = lm(CSSACC_V2p ~ CFCOR_V2p, data=df, na.action = "na.exclude")
+df$CSSACCADJ = residuals(mod.CSSACC) + coef(mod.CSSACC)[["(Intercept)"]]
+df$CSSACCADJ_V2p = residuals(mod.CSSACC_V2p) + coef(mod.CSSACC_V2p)[["(Intercept)"]]
 
-df$Verbal = df$zvoctran
-df$zVerbal = scale(df$Verbal)
-scaleValues = addScaleVals(scaleValues, "Verbal", df$zVerbal)
+# Z-score test scores
+df = scaleVals(df, "CSSACCADJ")
 
-# vetsa1 Visual-Spatial Ability 
-df$zMR1COR = scale(df$MR1COR)
-scaleValues = addScaleVals(scaleValues, "MR1COR", df$zMR1COR)
-df$zHFTOTCOR = scale(df$HFTOTCOR)
-scaleValues = addScaleVals(scaleValues, "HFTOTCOR", df$zHFTOTCOR)
+# Create domain scores
+df$zExecCategorySwitch = as.numeric(df[,"zCSSACCADJ"])
+df$zExecCategorySwitch_V2p = as.numeric(df[,"zCSSACCADJ_V2p"])
 
-df$VisSpat = rowMeans(df[c("zMR1COR","zHFTOTCOR")])
-df$zVisSpat = scale(df$VisSpat)
-scaleValues = addScaleVals(scaleValues, "VisSpat", df$zVisSpat)
+#---------------------------------#
+# Executive Function : Inhibition #
+#---------------------------------#
 
-# vetsa1 Abstract Reasoning 
-df$zMTXTRAN = scale(df$MTXTRAN)
-scaleValues = addScaleVals(scaleValues, "MTXTRAN", df$zMTXTRAN)
+# Filter out Stroop errors
+df$STRIT[which(df$STRIT=="1")] = NA
+df$STRIT[which(df$STRIT>90)] = NA
+df$STRIT_V2p[which(df$STRIT_V2p=="1")] = NA
+df$STRIT_V2p[which(df$STRIT_V2p>90)] = NA
 
-df$AbsReason = as.numeric(df$zMTXTRAN)
-df$zAbsReason = scale(df$AbsReason)
-scaleValues = addScaleVals(scaleValues, "AbsReason", df$zAbsReason)
+# Z-score test scores
+df = scaleVals(df, "STRIT")
+df$zExecInhibit = as.numeric(df[,"zSTRIT"])
+df$zExecInhibit_V2p = as.numeric(df[,"zSTRIT_V2p"])
 
-# Vetsa1 Working Memory 
-df$zdsfraw = scale(df$dsfraw)
-scaleValues = addScaleVals(scaleValues, "dsfraw", df$zdsfraw)
-df$zdsbraw = scale(df$dsbraw)
-scaleValues = addScaleVals(scaleValues, "dsbraw", df$zdsbraw)
-df$zlntot = scale(df$lntot)
-scaleValues = addScaleVals(scaleValues, "lntot", df$zlntot)
-df$zsspfraw = scale(df$sspfraw)
-scaleValues = addScaleVals(scaleValues, "sspfraw", df$zsspfraw)
-df$zsspbraw = scale(df$sspbraw)
-scaleValues = addScaleVals(scaleValues, "sspbraw", df$zsspbraw)
-df$zrsatotrevtran = scale(df$RSATOTrevtran) 
-scaleValues = addScaleVals(scaleValues, "RSATOTrevtran", df$zrsatotrevtran)
-
-df$STWKMem = rowMeans(df[,c("zdsfraw","zdsbraw",
-                                              "zlntot","zsspfraw",
-                                              "zsspbraw","zrsatotrevtran")])
-df$zSTWKMem = scale(df$STWKMem)
-scaleValues = addScaleVals(scaleValues, "STWKMem", df$zSTWKMem)
-
-# vetsa1 Episodic Memory 
-df$zcvatot = scale(df$cvatot)
-scaleValues = addScaleVals(scaleValues, "cvatot", df$zcvatot)
-df$zcvsdfr = scale(df$CVSDFR)
-scaleValues = addScaleVals(scaleValues, "CVSDFR", df$zcvsdfr)
-df$zcvldfr = scale(df$CVLDFR)
-scaleValues = addScaleVals(scaleValues, "CVLDFR", df$zcvldfr)
-df$zlmitot = scale(df$lmitot)
-scaleValues = addScaleVals(scaleValues, "lmitot", df$zlmitot)
-df$zlmdtot = scale(df$lmdtot)
-scaleValues = addScaleVals(scaleValues, "lmdtot", df$zlmdtot)
-df$zvritot = scale(df$vritot)
-scaleValues = addScaleVals(scaleValues, "vritot", df$zvritot)
-df$zvrdtot = scale(df$vrdtot)
-scaleValues = addScaleVals(scaleValues, "vrdtot", df$zvrdtot)
-
-df$EpsMem = rowMeans(df[,c("zcvatot","zcvsdfr",
-                                             "zcvldfr","zlmitot",
-                                             "zlmdtot","zvritot",
-                                             "zvrdtot")])
-df$zEpsMem = scale(df$EpsMem)
-scaleValues = addScaleVals(scaleValues, "EpsMem", df$zEpsMem)
-
-# Vetsa1 Verbal Fluency 
-df$zlfcor = scale(df$LFCOR)
-scaleValues = addScaleVals(scaleValues, "LFCOR", df$zlfcor)
-df$zcfcor = scale(df$CFCOR)
-scaleValues = addScaleVals(scaleValues, "CFCOR", df$zcfcor)
-
-df$VerbFlu = rowMeans(df[,c("zlfcor","zcfcor")])
-df$zVerbFlu = scale(df$VerbFlu)
-scaleValues = addScaleVals(scaleValues, "VerbFlu", df$zVerbFlu)
-
-# Vetsa1 Processing Speed
-df$zstrwraw = scale(df$strwraw)
-scaleValues = addScaleVals(scaleValues, "strwraw", df$zstrwraw)
-df$zstrcraw = scale(df$strcraw)
-scaleValues = addScaleVals(scaleValues, "strcraw", df$zstrcraw)
-df$ztrl2tran = scale(df$TRL2TRAN)
-scaleValues = addScaleVals(scaleValues, "TRL2TRAN", df$ztrl2tran)
-df$ztrl3tran = scale(df$TRL3TRAN)
-scaleValues = addScaleVals(scaleValues, "TRL3TRAN", df$ztrl3tran)
-
-df$ProcSpeed = rowMeans(df[,c("zstrwraw","zstrcraw",
-                                                "ztrl2tran","ztrl3tran")],na.rm=T)
-df$zProcSpeed = scale(df$ProcSpeed)
-scaleValues = addScaleVals(scaleValues, "ProcSpeed", df$zProcSpeed)
-
-# Vetsa1 Executive Functioning - Trails Switching 
-df$ztrl4adjtran = scale(df$TRL4ADJTRAN) 
-scaleValues = addScaleVals(scaleValues, "TRL4ADJTRAN", df$ztrl4adjtran)
-
-df$ExecTrailsSwitch = as.numeric(df$ztrl4adjtran )
-df$zExecTrailsSwitch = scale(df$ExecTrailsSwitch)
-scaleValues = addScaleVals(scaleValues, "ExecTrailsSwitch", df$zExecTrailsSwitch)
-
-# vetsa1 Executive Functioning - Category Switching
-df$zCSSACCADJ = scale(df$CSSACCADJ)
-scaleValues = addScaleVals(scaleValues, "CSSACCADJ", df$zCSSACCADJ)
-
-df$ExecCategorySwitch = as.numeric(df$zCSSACCADJ)
-df$zExecCategorySwitch = scale(df$ExecCategorySwitch)
-scaleValues = addScaleVals(scaleValues, "ExecCategorySwitch", df$zExecCategorySwitch)
-
-# vetsa1 Executive Functioing - Inhibition
-df$zstrit = scale(df$STRIT) 
-scaleValues = addScaleVals(scaleValues, "strit", df$zstrit)
-
-df$ExecInhibit = as.numeric(df$zstrit)
-df$zExecInhibit = scale(df$ExecInhibit)
-scaleValues = addScaleVals(scaleValues, "ExecInhibit", df$zExecInhibit)
 
 #-------------------#
 #  Save out datset  #
 #-------------------#
 
 # Select cognitive domain variables
-vetsa1CogDomains = df %>%
-  dplyr::select(vetsaid,zVerbal,zvoctran,zVisSpat,zMR1COR,zHFTOTCOR,
-                zSTWKMem,zdsfraw,zdsbraw,zlntot,zsspfraw,zsspbraw,
-                zrsatotrevtran,zEpsMem,zcvatot,zcvsdfr,zcvldfr,
-                zlmitot,zlmdtot,zvritot,zvrdtot,zAbsReason,zMTXTRAN,
-                zVerbFlu,zlfcor,zcfcor,zExecTrailsSwitch,ztrl4adjtran,
-                zProcSpeed,zstrwraw,zstrcraw,ztrl2tran,ztrl3tran,
-                zExecCategorySwitch,zCSSACCADJ,zExecInhibit,zstrit) 
+dfCogDomains = df %>%
+  dplyr::select(VETSAID,
+                zVisSpat,zMR1COR,zHFTOTCOR,
+                zSTWKMem,zDSFRAW,zDSBRAW,zLNTOT,zSSPFRAW,zSSPBRAW,zRSATOTTRAN,
+                zEpsMem,zCVATOT,zCVSDFR,zCVLDFR,zLMITOT,zLMDTOT,zVRITOT,zVRDTOT,
+                zAbsReason,zMTXTRAN,
+                zVerbFlu,zLFCOR,zCFCOR,
+                zProcSpeed,zSTRWRAW,zSTRCRAW,zTRL2TRAN,zTRL3TRAN,
+                zExecTrailsSwitch,zTRL4ADJTRAN,
+                zExecCategorySwitch,zCSSACCADJ,
+                zExecInhibit,zSTRIT,
+                zVisSpat_V2p,zMR1COR_V2p,zHFTOTCOR_V2p,
+                zSTWKMem_V2p,zDSFRAW_V2p,zDSBRAW_V2p,zLNTOT_V2p,zSSPFRAW_V2p,zSSPBRAW_V2p,zRSATOTTRAN_V2p,
+                zEpsMem,zCVATOT,zCVSDFR,zCVLDFR,zLMITOT,zLMDTOT,zVRITOT,zVRDTOT,
+                zAbsReason_V2p,zMTXTRAN_V2p,
+                zVerbFlu_V2p,zLFCOR_V2p,zCFCOR_V2p,
+                zProcSpeed_V2p,zSTRWRAW_V2p,zSTRCRAW_V2p,zTRL2TRAN_V2p,zTRL3TRAN_V2p,
+                zExecTrailsSwitch_V2p,zTRL4ADJTRAN_V2p,
+                zExecCategorySwitch_V2p,zCSSACCADJ_V2p,
+                zExecInhibit_V2p,zSTRIT_V2p) 
 
 # Save out data
-write.csv(vetsa1CogDomains, 
-          "K:/Projects/Cognitive Domains/data/V1_CognitiveDomains.csv",
-          row.names = F)
-
-# Save out Means and SDs for use in scaling Vetsa 2 data
-write.csv(scaleValues, 
-          "/home/jelman/netshare/K/Projects/Cognitive Domains/data/V1_CognitiveDomains_Means_SDs.csv",
+write.csv(dfCogDomains, 
+          "~/netshare/M/PSYCH/KREMEN/Practice Effect Cognitive Domains/data/V1V2_CognitiveDomains_PracEffects.csv",
           row.names = F)
